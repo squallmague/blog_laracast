ef7c1200708aa0932b5ca165224fc5ac56fe055c
squallmague <ibrahimzerpa@gmail.com>
2017-10-21 17:14:33 +0000

    8 controllers

---
+++ app/Task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public function scopeIncomplete($query)
+
+    {
+
+    	return $query->where('completed', 0);
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/tasks', 'TasksController@index');
+
+Route::get('/tasks/{task}', 'TasksController@show');
